704e9c5545b0cb2c6e2d5b3515a6aa5e6612d33c
Hagen Rother <hagen.rother@liquidm.com>
2016-10-05 12:58:35 +0200

    hack to kafka 0.10

--- bin/gobblin-mapreduce.sh
+++ bin/gobblin-mapreduce.sh
@@ -142,8 +142,8 @@ LIBJARS=(
   $FWDIR_LIB/data-1.15.9.jar
   $FWDIR_LIB/gson-2.6.2.jar
   $FWDIR_LIB/joda-time-2.9.3.jar
-  $FWDIR_LIB/kafka_2.11-0.8.2.2.jar
-  $FWDIR_LIB/kafka-clients-0.8.2.2.jar
+  $FWDIR_LIB/kafka_2.11-0.10.0.1.jar
+  $FWDIR_LIB/kafka-clients-0.10.0.1.jar
   $FWDIR_LIB/metrics-core-2.2.0.jar
   $FWDIR_LIB/metrics-core-3.1.0.jar
   $FWDIR_LIB/metrics-graphite-3.1.0.jar
