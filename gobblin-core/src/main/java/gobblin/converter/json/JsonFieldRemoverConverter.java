971d622c7daa8600e0ad187102a286db8846aa56
Krzysztof Kotlarski <kotlarski.krzysztof@gmail.com>
2018-05-24 14:03:53 +0200

    Use byte[] instead of string

--- gobblin-core/src/main/java/gobblin/converter/json/JsonFieldRemoverConverter.java
+++ gobblin-core/src/main/java/gobblin/converter/json/JsonFieldRemoverConverter.java
@@ -1,6 +1,7 @@
 package gobblin.converter.json;
 
 import java.util.List;
+import java.nio.charset.StandardCharsets;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
@@ -18,16 +19,16 @@ import gobblin.converter.SchemaConversionException;
 /**
  * A {@link Converter} to remove JSON attributes from JSON string.
  */
-public class JsonStringFieldRemoverConverter extends Converter<String, String, String, String> {
+public class JsonFieldRemoverConverter extends Converter<String, String, byte[], byte[]> {
   private List<String> fields;
 
   private static Gson GSON = new Gson();
   private static final Splitter SPLITTER_ON_COMMA = Splitter.on(',').trimResults().omitEmptyStrings();
 
   @Override
-  public Converter<String, String, String, String> init(WorkUnitState workUnit) {
+  public Converter<String, String, byte[], byte[]> init(WorkUnitState workUnit) {
     Preconditions.checkArgument(workUnit.contains(ConfigurationKeys.CONVERTER_JSON_REMOVE_FIELDS),
-        "Missing required property converter.json.remove.fields for the JsonStringSchemaFieldRemoverConverter class.");
+        "Missing required property converter.json.remove.fields for the JsonFieldRemoverConverter class.");
     this.fields = SPLITTER_ON_COMMA.splitToList(workUnit.getProp(ConfigurationKeys.CONVERTER_JSON_REMOVE_FIELDS));
     return super.init(workUnit);
   }
@@ -40,14 +41,15 @@ public class JsonStringFieldRemoverConverter extends Converter<String, String, S
   }
 
   @Override
-  public Iterable<String> convertRecord(String outputSchema, String inputRecord, WorkUnitState workUnit)
+  public Iterable<byte[]> convertRecord(String outputSchema, byte[] inputRecord, WorkUnitState workUnit)
       throws DataConversionException {
-        JsonObject record = GSON.fromJson(inputRecord, JsonObject.class);
+        String payload = (inputRecord == null) ? "" : new String(inputRecord, StandardCharsets.UTF_8);
+        JsonObject record = GSON.fromJson(payload, JsonObject.class);
         for (String key : fields) {
           record.remove(key);
         }
 
-    return Lists.newArrayList(GSON.toJson(record));
+    return Lists.newArrayList(GSON.toJson(record).getBytes(StandardCharsets.UTF_8));
   }
 
 }
